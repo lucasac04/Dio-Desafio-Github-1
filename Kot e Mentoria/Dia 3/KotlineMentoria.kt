637129bc373187e1e5ef6775d057ccdda86c773b
Loucong <loucongxd@gmail.com>
2022-07-19 10:31:18 -0300

    Volta aos estudos

--- Kot e Mentoria/Dia 3/KotlineMentoria.kt
+++ Kot e Mentoria/Dia 3/KotlineMentoria.kt
@@ -1,26 +1,31 @@
 fun main (){
     
-    val x:Int = 5
-    val y:Int = 2
-    val op = listOf(::sum, ::sub, ::mult, ::division)
-    val opChoosed = 3 //U can change the operation here
-    val result:Int
+    val x:Float? = 5.toFloat()
+    val y:Float? = 2.toFloat()
+    val op = listOf(::sum, ::sub, ::mult, ::porcentagem, ::division)
+    val opChoosed = 4//U can change the operation here
+    val result:Float
     
-    result = calculate(x, y, op[opChoosed])
-	println(result)
+    when{
+    	x==null -> {println("O primeiro valor é nulo")}
+        y==null -> {println("O segundo valor é nulo")}
+        else -> {result = calculate(x, y, op[opChoosed])
+				println(result)
+        }
+    }
     
     
 }
 
-fun sum(a1:Int, a2:Int) = a1.plus(a2)
-fun sub(a1:Int, a2:Int) = a1.minus(a2)
-fun mult(a1:Int, a2:Int) = a1.times(a2)
-fun division(a1:Int, a2:Int) = a1.div(a2)
-fun calculate(n1:Int, n2:Int, operation:(Int,Int)->Int):Int{
+fun sum(a1:Float, a2:Float) = a1.plus(a2)
+fun sub(a1:Float, a2:Float) = a1.minus(a2)
+fun mult(a1:Float, a2:Float) = a1.times(a2)
+fun porcentagem(a1:Float, a2:Float) = a2.div(a1)*100
+fun division(a1:Float, a2:Float) = a1.div(a2)
+fun calculate(n1:Float, n2:Float, operation:(Float,Float)->Float):Float{
     
     val result = operation(n1, n2)
     return result
     
 }
-
 //Base de uma calculadora, será melhorada de acordo com o avanço dos estudos, preciso aprender ainda métodos de interação com o usuário
